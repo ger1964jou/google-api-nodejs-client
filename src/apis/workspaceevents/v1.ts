a2853fa587cd9bdef40c0c504bd816429ae171c6
Yoshi Automation <yoshi-automation@google.com>
2024-04-07 01:34:40 +0000

    fix(workspaceevents): update the API
    #### workspaceevents:v1
    
    The following keys were changed:
    - resources.subscriptions.methods.patch.parameters.updateMask.description

--- src/apis/workspaceevents/v1.ts
+++ src/apis/workspaceevents/v1.ts
@@ -958,7 +958,7 @@ export namespace workspaceevents_v1 {
      */
     name?: string;
     /**
-     * Optional. Required. The field to update. You can update one of the following fields in a subscription: * `expire_time`: The timestamp when the subscription expires. * `ttl`: The time-to-live (TTL) or duration of the subscription.
+     * Optional. The field to update. If omitted, updates any fields included in the request. You can update one of the following fields in a subscription: * `expire_time`: The timestamp when the subscription expires. * `ttl`: The time-to-live (TTL) or duration of the subscription. To fully replace the subscription (the equivalent of `PUT`), use `*`. Any omitted fields are updated with empty values.
      */
     updateMask?: string;
     /**
